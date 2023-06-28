2bff6ee2d1b88d668c2a5934bde7b9dbf50914ce
Luan Araujo <luangoncalves9@hotmail.com>
2023-06-28 17:53:22 -0300

    interface criada já no streamlit

--- tabela.py
+++ tabela.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+import base64
+import re
+import locale
+
+# Configuração da formatação de moeda
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 # Caminho do arquivo CSV da tabela base
 caminho_tabela_base = 'teste/base.csv'
@@ -26,17 +32,43 @@ def tratar_tabela(caminho_arquivo):
     colunas_selecionadas = selecionar_colunas(df)
     df_selecionado = df[colunas_selecionadas]
 
-    # Mescla com a tabela base
-    df_final = pd.concat([df_base, df_selecionado], axis=1)
+    # Verifica as variações de nomes das colunas de código e valor
+    coluna_codigo = [
+        col for col in df_selecionado.columns if 'codigo' in col.lower()][0]
+    coluna_valor = [
+        col for col in df_selecionado.columns if 'valor' in col.lower()][0]
 
-    # Solicita o caminho para salvar o arquivo tratado
-    caminho_salvar = st.text_input(
-        'Digite o caminho para salvar o arquivo tratado', value='arquivo_tratado.csv')
+    # Atualiza as colunas "código" e "ch" da tabela base
+    df_base['código'] = df_selecionado[coluna_codigo].apply(
+        lambda x: re.sub(r'Código:', '', str(x)) if pd.notnull(x) else '')
+    df_base['ch'] = df_selecionado[coluna_valor].apply(
+        lambda x: format_currency(x) if pd.notnull(x) else '')
 
-    # Salva o DataFrame tratado como um arquivo CSV
-    df_final.to_csv(caminho_salvar, sep=';', quoting=1, index=False)
+    # Cria uma cópia da tabela base com as colunas atualizadas
+    df_final = df_base.copy()
 
-    st.write('Tabela tratada e salva com sucesso!')
+    # Codifica o DataFrame tratado em CSV para download
+    csv = df_final.to_csv(sep=';', quoting=1, index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="arquivo_tratado.csv">Baixar arquivo tratado</a>'
+
+    st.markdown(href, unsafe_allow_html=True)
+    st.write('Tabela tratada gerada com sucesso!')
+
+# Função para formatar o valor da coluna "ch"
+
+
+def format_currency(value):
+    if pd.notnull(value):
+        value = str(value)
+        if value.startswith('RS '):
+            value = value[3:]
+        value = re.sub(r'[^0-9,.]', '', value)
+        try:
+            value = locale.currency(float(value), grouping=True, symbol=False)
+        except ValueError:
+            pass
+    return value
 
 
 # Interface Streamlit
